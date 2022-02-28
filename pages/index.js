2175f697ed3a895df6c7b251c9779e63f09af365
Tyus Durant <todurant@ncdot.gov>
2022-02-28 13:23:22 -0500

    1323_02282023

--- pages/index.js
+++ pages/index.js
@@ -1,53 +1,112 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import {useState, useEffect} from 'react'
+import Link from 'next/link'
+const defaultEndpoint = "https://rickandmortyapi.com/api/character/";
 
-export default function Home() {
+export async function getServerSideProps(){
+  const res = await fetch(defaultEndpoint);
+  const data = await res.json();
+  return{
+    props:{
+      data
+    }
+  }
+}
+
+export default function Home({data}) {
+  //console.log(data);
+  const {info, results:defaultResults = []} = data;
+  const [results, updateResults] = useState(defaultResults);
+  const [page, updatePage] = useState({...info, current:defaultEndpoint});
+  const {current} = page;
+
+  useEffect(() => {
+    if(current === defaultEndpoint)return;
+    async function request(){
+      const res = await fetch(current);
+      const nextData = await res.json();
+
+      updatePage({
+        current,
+        ...nextData.info
+      });
+
+      if(!nextData.info?.prev){
+        updateResults(nextData.results);
+        return;
+      }
+
+      updateResults(prev => {
+        return[
+          ...prev,
+          ...nextData.results
+        ]
+      });
+    }
+    request();
+  }, [current]);
+
+  function handleLoadMore(){
+    updatePage(prev => {
+      return {
+        ...prev,
+        current: page?.next
+      }
+    })
+  }
+
+  function handleOnSubmitSearch(e){
+    e.preventDefault();
+    const { currentTarget = {}} = e;
+    const fields = Array.from(currentTarget?.elements);
+    const fieldQuery = fields.find(field => field.name === 'query');
+
+    const value = fieldQuery.value || '';
+    const endpoint = `https://rickandmortyapi.com/api/character/?name=${value}`;
+
+    updatePage({current:endpoint});
+  }
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>Wubba Lubba Dub dub</title>
+        
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Wubba Lubba Dub dub
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          Rick and Morty Wiki
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <form className={styles.search} onSubmit={handleOnSubmitSearch}>
+          <input name="query" type="search"/>
+          <button>Search</button>
+        </form>
+        <ul className={styles.grid}>
+          {results.map(results => {
+            const {id, name, image} = results;
+            return (
+              <li className={styles.card} key={id}>
+                <Link href="/character/[id" as={`/character/${id}`}>
+                  <a >
+                    <img src={image} alt={name}/>
+                    <h3>{name}</h3>
+                    
+                  </a>
+                </Link>
+                
+              </li>
+            )
+          })}   
+        </ul>
+        <p>
+          <button onClick={handleLoadMore}>Load more...</button>
+        </p>
       </main>
 
       <footer className={styles.footer}>
